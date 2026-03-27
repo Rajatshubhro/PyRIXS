03ec07109056a9197d021fb131ee38f25fa598b5
Rajatshubhro <majumder.rajat071@gmail.com>
2026-03-27 15:31:06 -0700

    Added RIXS Kramer-Heisenberg formulation for CI tDMS

--- rixsci.py
+++ rixsci.py
@@ -21,88 +21,13 @@ HARTREE_PER_EV = 1.0 / EV_PER_HARTREE
 '''
 Provide Xn and Xf from two separate TD-DFT/TDA calcs
 '''
-def s_amplitudes(mf, Xn, Xf, nslice: list = None):
+def s_amplitudes(tdm_fn, tdm_ng):
 
-    nocc = Xf.shape[0]
-    nvirt = Xf.shape[1]
-    nmo = nocc + nvirt
-    if (not nslice):
-        nslice = [i for i in range(nocc)]
-        Xn_ncore = nocc
-    else:
-        Xn_ncore = Xn.shape[0]
-
-    # MO-basis dipole
-    ao_dipole = mf.mol.intor('int1e_r')
-    mo_dipole = np.einsum('pi,kpq,qj->kij', mf.mo_coeff, ao_dipole, mf.mo_coeff)
-    
-    # Get the tdms:
-    tdm_nf = np.einsum('ia,ib->ab', Xn, Xf[:Xn_ncore, :], optimize=True)
-    tdm_fn = np.einsum('ia,ja->ij', Xn, Xf, optimize=True)
-    # Amplitudes
-    ng_amp = np.einsum('kia,ia->k', mo_dipole[:, nslice, nocc : nmo], Xn, optimize=True)
-    fn_amp = np.einsum('kba,ba->k', mo_dipole[:, nocc : nmo, nocc : nmo], tdm_nf, optimize=True)
-    fn_amp -= np.einsum('kij,ij->k', mo_dipole[:, nslice, :nocc], tdm_fn, optimize=True)
-
-    # Obtain S_fn matrix:
-    S_fn =  np.outer(fn_amp, ng_amp)
-    # Kramers-Heisenberg formula for RIXS
+    S_fn =  np.outer(tdm_fn, tdm_ng)
     f = 0.
     f = ((2/15) * np.sum(S_fn**2)) - ((1/30) * ((np.trace(S_fn))**2 + np.sum(S_fn * S_fn.T)))
 
     return f
-
-''' 
-Provide ranges for:
-incident_en --> (500, 520)
-en_tranfer --> (-2, 15)
-broad_factor, fwhm --> Optional args
-Defaults:
-    step_size = 0.1 eV
-    broad_factor = 2.4 eV
-    fwhm = 1.2 eV
-'''
-def rixs_map(incident_en: tuple[float, float], en_transfer: tuple[float, float], fn_ampMatrix, en_vec, ef_vec, step_size = 0.1, broad_factor = 2.4, fwhm = 1.2):
-
-    if not isinstance(incident_en, tuple) or len(incident_en) != 2:
-        raise TypeError("Expected a pair (2-element tuple) for incident energy")
-    if not isinstance(en_transfer, tuple) or len(en_transfer) != 2:
-        raise TypeError("Expected a pair (2-element tuple) for energy transfer")
-   
-    alpha_ = 1 / 137.036
-    broad_factor /= 27.2114
-    sigma_ev = fwhm / (27.2114 * 2 * np.sqrt(2 * np.log(2)))
-    gaussian_broadening = lambda en_transfer, ef: np.exp(-0.5 * ((en_transfer - ef) / sigma_ev)**2)
-
-    init_en, final_en = incident_en
-    nsteps = int((final_en - init_en) // step_size)
-    en_iter = np.linspace(init_en, final_en, nsteps)
-    en_iter /= 27.2114 # Conversion to a.u
-
-    init_entrans, final_entrans = en_transfer
-    nsteps = int((final_entrans - init_entrans) // step_size)
-    entrans_iter = np.linspace(init_entrans, final_entrans, nsteps)
-    entrans_iter /= 27.2114 # Conversion to a.u
-
-    rixs_intensity_map = np.zeros((len(en_iter), len(entrans_iter)))
-
-    for i, en_inc in enumerate(en_iter):
-        for j, en_trans in enumerate(entrans_iter):
-            
-            en_emit = en_inc - en_trans
-            prefactor = en_emit / en_inc
-
-            result = 0.
-            for nf, en_f in enumerate(ef_vec):
-                gauss_bf = gaussian_broadening(en_trans, en_f)
-                for nn, en_n  in enumerate(en_vec):
-                    
-                    denom = ((en_inc - en_n)**2) + ((broad_factor**2) / 4.)
-                    result += np.abs(fn_ampMatrix[nf, nn]) * (((en_f * en_n * alpha_)**2) / denom) * gauss_bf
-
-            rixs_intensity_map[i,j] = result
-
-    return rixs_intensity_map,
 
 def efficient_rixs_map(
     incident_en: tuple[float, float],
@@ -148,46 +73,6 @@ def efficient_rixs_map(
     prefactor = (en_iter[:, None] - entrans_iter[None, :]) / en_iter[:, None]  # (Ni, Nj)
 
     return prefactor * rixs_intensity_map, en_iter, entrans_iter
-
-'''
-coreidx and viridx are optional args.
-If set to None, they include all the core / virt orbs respectively.
-Example:
-    coreidx --> [0,1,2,3]
-    viridx  --> [6,7,8,9]
-'''
-def select_orbitals_dft(mf, coreidx: list = None, viridx: list = None):
-    
-    if (coreidx == None):
-        coreidx = np.where(mf.mo_occ == 2)[0].tolist()
-    if (viridx == None):
-        viridx = np.where(mf.mo_occ == 0)[0].tolist()
-            
-    actidx = coreidx + viridx
-    mf_act = copy.deepcopy(mf)
-    mf_act.mo_coeff = mf.mo_coeff[:,actidx]
-    mf_act.mo_energy = mf.mo_energy[actidx]
-    mf_act.mo_occ = mf.mo_occ[actidx]
-    mf_act.direct_scf = False
-    mf_act._opt = {}
-
-    return mf_act
-
-'''
-Easy Run TD-DFT TDA:
-nstates in optional parameter
-
-'''
-def run_tda(mf, nstates=1):
-
-    tda = tddft.TDA(mf)
-    tda.nstates = nstates
-    tda.verbose = 4
-    tda.kernel()
-    tda.analyze()
-    
-    return tda
-
 
 '''
 if "main" : Runs test on RIXS
@@ -238,7 +123,7 @@ if __name__ == "__main__":
 
             Xn = tdn.xy[nn][0]
             Xf = tdf.xy[nf][0]
-            s_fnMatrix[nf,nn] =  s_amplitudes(mf, Xn, Xf, core_slice)
+            s_fnMatrix[nf,nn] =  s_amplitudes(mf, tdm_fn, tdm_ng)
             en_n[nn] = tdn.e[nn]
 
     # Parameters for RIXS Map:
